e2ca839e5e5054e0cde166b4939b70b4b866cb8d
Kauna <kaunalei@gmail.com>
2020-07-01 01:51:51 -0700

    Fixed error should build sucessfully

--- 00_first_example/main_test.go
+++ 00_first_example/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestAdd1(t *testing.T) {
-	result := add1(3)
+	result := add1(2)
 	if result != 2 {
 		t.Errorf("Wrong sum, got result: %d, but instead want: %d", result, 2)
 	}
